d649a116cbc6f6da7057be306a19688c99ee0a86
Mohamed Elsamny <mohamedsamny@gmail.com>
2021-02-28 01:23:56 -0500

    Read DB Name from env variable

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const username = process.env.DB_USERNAME || "admin";
 const password = process.env.DB_PASSWORD;
-const uri = `mongodb+srv://${username}:${password}@cluster0.mfetu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const databaseName = process.env.DB_NAME || "vending-machine";
+const uri = `mongodb+srv://${username}:${password}@cluster0.mfetu.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
 module.exports = function (app) {
     mongoose.connect(uri,{ useCreateIndex: true, useNewUrlParser: true });
