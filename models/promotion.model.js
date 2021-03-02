07f358f805b84914569c0c24956569690a51f95a
Mohamed Elsamny <mohamedsamny@gmail.com>
2021-03-02 12:01:29 -0500

    Add eslint

---
+++ models/promotion.model.js
@@ -0,0 +1,14 @@
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient')
+  const { Schema } = mongooseClient
+  const { ObjectId } = Schema
+
+  const promotion = new Schema({
+    id: ObjectId,
+    product_id: String,
+    start_date: Date,
+    end_date: Date
+  })
+
+  return mongooseClient.model('promotion', promotion)
+}
