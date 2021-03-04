a71d504f0d0893c9805ce498d8a013eff901baaf
Mohamed Elsamny <mohamedsamny@gmail.com>
2021-03-04 14:56:41 -0500

    refill endpoint

--- controllers/api/products.js
+++ controllers/api/products.js
@@ -13,4 +13,13 @@ module.exports = function (app) {
     const products = await models.product.find()
     res.json(products)
   })
+
+  app.post('/api/products/:id/refill', async function (req, res) {
+    const product = await models.product.findById(req.params.id)
+    product.quantity += 1
+    await product.save()
+
+    const products = await models.product.find()
+    res.json(products)
+  })
 }
