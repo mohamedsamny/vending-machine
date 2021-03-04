430b249751ff53602acdfa0c8849fc32a2439744
Mohamed Elsamny <mohamedsamny@gmail.com>
2021-03-04 09:11:22 -0500

    CORS fix

--- controllers/api/products.js
+++ controllers/api/products.js
@@ -2,6 +2,6 @@ module.exports = function (app) {
   const models = app.get('models')
   app.get('/api/products', async function (req, res) {
     const products = await models.product.find()
-    res.send(products)
+    res.json(products)
   })
 }
--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || '3000'
 const mongoose = require('./mongoose')
+const cors = require('cors')
 mongoose(app)
+
 const models = require('./models')(app)
 app.set('models', models)
+app.use(cors())
+
 require('./controllers')(app)
 
 app.listen(port, function () {
